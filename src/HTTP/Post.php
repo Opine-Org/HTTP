306a6a065405e6117fdef1db97617a8a6d0adc75
webuser <ryan@virtuecenter.com>
2013-10-15 18:39:14 -0400

    finish post and response

--- src/HTTP/Post.php
+++ src/HTTP/Post.php
@@ -2,5 +2,49 @@
 namespace HTTP;
 
 class Post {
-	
+	private $endpoint;
+	private $post = [];
+	private $status = 'empty';
+	private $errors = [];
+
+	public function __get ($field) {
+		if (!isset($this->post[$field])) {
+			return false;
+		}
+		return $this->post[$field];
+	}
+
+	public function populate ($endpoint, Array $post) {
+		$this->endpoint = $endpoint;
+		$this->post = new \ArrayObject($post);
+		foreach ($this->post as $key => &$value) {
+			if (is_array($value)) {
+				$value = new \ArrayObject($value);
+			}
+		}
+		$this->status = 'populated';
+	}
+
+	public function statusSaved () {
+		$this->status = 'saved';
+	}
+
+	public function statusCheck () {
+		return $this->status;
+	}
+
+	public function errorFieldSet ($marker, $field, $error) {
+		if (empty($field)) {
+			throw new \Exception('Can not set error on blank field.');
+		}
+		if (!isset($this->errors[$marker])) {
+			$this->errors[$marker] = $marker;
+		}
+		$this->errors[$marker][$field] = $error;
+		$this->status = 'error';
+	}
+
+	public function errorsGet () {
+		return $this->errors;
+	}
 }
--- src/HTTP/Response.php
+++ src/HTTP/Response.php
@@ -2,15 +2,11 @@
 namespace HTTP;
 
 class Response {
-	private $body = '';
-	private $mimeType = '';
-	private $status = 200;
+	public $body = '';
+	public $mimeType = 'text/html';
+	public $status = 200;
 
 	public function __toString () {
 		return $this->body;
-	}
-
-	public function body ($body) {
-		$this->body = $body;
 	}
 }
